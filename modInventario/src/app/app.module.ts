277c4782ce9bffcddbe1f9ba0acdf315b6b67eb7
zarek <carolina.mendoza@epn.edu.ec>
2022-07-18 19:11:06 -0500

    conf firebase y pantalla log-in

--- modInventario/src/app/app.module.ts
+++ modInventario/src/app/app.module.ts
@@ -9,6 +9,17 @@ import {MatDialogModule} from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NavBarModule} from 'src/app/componentes/nav-bar/nav-bar.module';
 
+//importar modules de firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
+// Auth service
+import { AuthService } from "./servicios/auth.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,8 +32,13 @@ import {NavBarModule} from 'src/app/componentes/nav-bar/nav-bar.module';
     MatDialogModule,
     BrowserAnimationsModule,
     NavBarModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- modInventario/src/environments/environment.ts
+++ modInventario/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBt7CZOJkpTUefuVXWmQIIFAx0KnYafAFU",
+    authDomain: "bd-inventario-795da.firebaseapp.com",
+    projectId: "bd-inventario-795da",
+    storageBucket: "bd-inventario-795da.appspot.com",
+    messagingSenderId: "297250843275",
+    appId: "1:297250843275:web:2cbcc677f85c292b6dc28e",
+    measurementId: "G-R3TX832HVJ"
+  }
 };
 
 /*
