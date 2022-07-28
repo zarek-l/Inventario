9873df9f37a05c1dab247d163dd12d563e8c1ec7
zarek <carolina.mendoza@epn.edu.ec>
2022-07-27 21:11:19 -0500

    logica crear bodegas

--- modInventario/src/app/rutas/ruta-crear-bodega/ruta-crear-bodega.component.ts
+++ modInventario/src/app/rutas/ruta-crear-bodega/ruta-crear-bodega.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {initializeApp} from "firebase/app";
+import {environment} from "../../../environments/environment";
+import {addDoc, collection, getFirestore} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-ruta-crear-bodega',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-crear-bodega.component.scss']
 })
 export class RutaCrearBodegaComponent implements OnInit {
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  mensaje: string  ="";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async crearOrden(nombre_bodega:string, direccion:string) {
+    let docRef = await addDoc(collection(this.db, "bodegas"), {
+      nombre_bodega: nombre_bodega,
+      direccion: direccion
+    });
+    this.mensaje = "Bodega registrada"
   }
 
 }
