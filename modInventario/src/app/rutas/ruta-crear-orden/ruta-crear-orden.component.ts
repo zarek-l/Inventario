2ce4107f6527aa3ff3f9a51d701f1c6821881201
zarek <carolina.mendoza@epn.edu.ec>
2022-07-27 21:02:57 -0500

    logica crear orden

--- modInventario/src/app/rutas/ruta-crear-orden/ruta-crear-orden.component.ts
+++ modInventario/src/app/rutas/ruta-crear-orden/ruta-crear-orden.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {initializeApp} from "firebase/app";
+import {environment} from "../../../environments/environment";
+import {addDoc, collection, getDocs, getFirestore} from "@angular/fire/firestore";
+import {DocumentData} from 'firebase/firestore';
 
 @Component({
   selector: 'app-ruta-crear-orden',
@@ -6,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-crear-orden.component.scss']
 })
 export class RutaCrearOrdenComponent implements OnInit {
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  mensaje: string  ="";
+  descripcion :DocumentData[]  =[];
+  razon_social :DocumentData[]  =[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.obtenerProducto()
+    this.obtenerProveedor()
   }
+
+  async obtenerProducto(){
+    let productoCol = collection(this.db, 'productos');
+    let productoSnapshot = await getDocs(productoCol);
+    this.descripcion = productoSnapshot.docs.map(doc => doc.data());
+  }
+
+  async obtenerProveedor(){
+    let proveedorCol = collection(this.db, 'proveedores');
+    let proveedorSnapshot = await getDocs(proveedorCol);
+    this.razon_social = proveedorSnapshot.docs.map(doc => doc.data());
+  }
+
+  async crearOrden(proveedor:string, producto:string,fecha:string, cantidad:number, total:number) {
+    let docRef = await addDoc(collection(this.db, "orden_compra"), {
+      proveedor: proveedor,
+      producto: producto,
+      fecha: fecha,
+      cantidad: cantidad,
+      total: total,
+
+    });
+    this.mensaje = "Orden de compra registrada"
+  }
+
 
 }
