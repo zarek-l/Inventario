bd0c486c13deaa66ccd4d2ddbb0e44a761d3ecb3
zarek <carolina.mendoza@epn.edu.ec>
2022-07-27 19:33:58 -0500

    logica agregar producto

--- modInventario/src/app/rutas/ruta-crear-producto/ruta-crear-producto.component.ts
+++ modInventario/src/app/rutas/ruta-crear-producto/ruta-crear-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {addDoc, collection, getFirestore} from "@angular/fire/firestore";
+import {environment} from "../../../environments/environment";
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-ruta-crear-producto',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-crear-producto.component.scss']
 })
 export class RutaCrearProductoComponent implements OnInit {
-
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  mensaje: string  ="";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  async crearProducto(descripcion:string, categoria:string,precio:number) {
+    let docRef = await addDoc(collection(this.db, "productos"), {
+      descripcion: descripcion,
+      categoria: categoria,
+      precio: precio,
+    });
+    this.mensaje = "Publicación realizada"
+  }
 }
