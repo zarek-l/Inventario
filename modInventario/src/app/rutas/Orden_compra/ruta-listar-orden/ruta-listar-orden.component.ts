313de33aa4c42fc26b376b58f82b02210d1fa843
zarek <carolina.mendoza@epn.edu.ec>
2022-08-01 17:30:33 -0500

    logica listar ordenes

--- modInventario/src/app/rutas/Orden_compra/ruta-listar-orden/ruta-listar-orden.component.ts
+++ modInventario/src/app/rutas/Orden_compra/ruta-listar-orden/ruta-listar-orden.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {initializeApp} from "firebase/app";
+import {environment} from "../../../../environments/environment";
+import {collection, getDocs, getFirestore, query} from "@angular/fire/firestore";
+import {DocumentData} from "firebase/firestore";
 
 @Component({
   selector: 'app-ruta-listar-orden',
   templateUrl: './ruta-listar-orden.component.html',
   styleUrls: ['./ruta-listar-orden.component.scss']
 })
-export class RutaListarOrdenComponent implements OnInit {
 
-  constructor() { }
+export class RutaListarOrdenComponent implements OnInit {
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  ordenes: DocumentData[] = [];
+  constructor(
+  ) { }
 
   ngOnInit(): void {
+    this.obtenerOrdenes()
+  }
+  async obtenerOrdenes(){
+    this.ordenes = []
+    let OrdCol = collection(this.db, 'orden_compra');
+    let OrdSnapshot =  query(OrdCol);
+    let OrdQuery = await getDocs(OrdSnapshot)
+    this.ordenes = OrdQuery.docs.map(doc => doc.data())
   }
 
 }
