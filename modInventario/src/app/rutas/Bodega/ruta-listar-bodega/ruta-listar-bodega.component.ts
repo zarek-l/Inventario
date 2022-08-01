c081eeb8f073f77d8a7995e9b5aeb97654e48ac1
zarek <carolina.mendoza@epn.edu.ec>
2022-08-01 17:36:14 -0500

    logica listar bodegas

--- modInventario/src/app/rutas/Bodega/ruta-listar-bodega/ruta-listar-bodega.component.ts
+++ modInventario/src/app/rutas/Bodega/ruta-listar-bodega/ruta-listar-bodega.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {initializeApp} from "firebase/app";
+import {environment} from "../../../../environments/environment";
+import {collection, getDocs, getFirestore, query} from "@angular/fire/firestore";
+import {DocumentData} from "firebase/firestore";
 
 @Component({
   selector: 'app-ruta-listar-bodega',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-listar-bodega.component.scss']
 })
 export class RutaListarBodegaComponent implements OnInit {
-
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  bodegas: DocumentData[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.obtenerOrdenes()
+  }
+  async obtenerOrdenes(){
+    this.bodegas = []
+    let BodCol = collection(this.db, 'bodegas');
+    let BodSnapshot =  query(BodCol);
+    let BodQuery = await getDocs(BodSnapshot)
+    this.bodegas = BodQuery.docs.map(doc => doc.data())
   }
 
 }
